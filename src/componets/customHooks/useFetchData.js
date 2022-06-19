adcd3bde19994512c70fd15e3f0db24eb124c000
Bineeshthekyal <bineeshthekyal@gmail.com>
2022-06-19 21:46:25 +0530

    change line in fetch component

--- src/componets/customHooks/useFetchData.js
+++ src/componets/customHooks/useFetchData.js
@@ -14,7 +14,19 @@ const useFetchData =(url) =>{
     },
      [url]
     );
-  
+    // const useFetchData =(url) =>{
+
+    //   const [data, setData] =useState([]);
+    
+    
+    //   useEffect(()=>{
+    
+    //     fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => setData(data));
+    //   },
+    //    [url]
+    //   );
   return [data];
   
   };
